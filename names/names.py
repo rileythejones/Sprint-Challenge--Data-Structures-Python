f7ce1a91bcc37220058d53572cfd1e77a1aff66e
rileythejones <rileymjones@gmail.com>
2020-04-24 12:03:04 -0600

    sprint challenge

--- names/names.py
+++ names/names.py
@@ -12,11 +12,63 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+
+""" runtime complexity n^2 """
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+
+""" runtime complexity log_2(n) """
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+
+            if self.left:
+                self.left.insert(value)
+            else: 
+                self.left = BinarySearchTree(value)
+
+        else:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BinarySearchTree(value)
+
+    def contains(self, target):
+        node = self
+        while node:
+            if target == node.value:
+                return True
+            else:
+                if target > node.value:
+                    node = node.right
+                else:
+                    node = node.left
+        return False
+
+BST = BinarySearchTree(names_1[0])
+
+for x in range(1, len(names_1)):
+    BST.insert(names_1[x])
+
+
+for x in range(0, len(names_2)):
+    if BST.contains(names_2[x]):
+        duplicates.append(names_2[x])
+
+
+
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -26,3 +78,7 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+
+names = [name for name in names_1 if name in names_2]
+print(names)
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,27 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        
+        if len(self.storage) < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.head
+        else:
+            if self.current.next:
+                self.current.value = item
+                self.current = self.current.next
+            else:
+                self.storage.tail.value = item
+                self.current = self.storage.head
 
+            
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        current = self.storage.head
+        while current:
+            list_buffer_contents.append(current.value)
+            current = current.next 
 
         return list_buffer_contents
 
